421096e82c7010268ce91b6b6dbb5754f8c6b083
corp11 <Cjatherton19@gmail.com>
2022-05-18 14:31:59 -0400

    Open Ai

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 require('dotenv').config();//This will allow you to get env key
 
